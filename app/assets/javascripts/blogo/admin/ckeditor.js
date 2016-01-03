ffbf4963a72fc47a36f5795d83177aad165452c3
jonybang <jonybang@mail.ru>
2016-01-04 00:10:07 +1000

    ckeditor

--- app/assets/javascripts/blogo/admin/ckeditor.js
+++ app/assets/javascripts/blogo/admin/ckeditor.js
@@ -1,4 +1,4 @@
-//= require blogo/ckeditor/ckeditor
 //= require blogo/ckeditor/ckeditor_basepath
+// = require blogo/ckeditor/ckeditor
 //= require blogo/ckeditor/config
 //= require blogo/ckeditor/lang/ru
