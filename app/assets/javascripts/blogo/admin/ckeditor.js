b6a2b4c897e7abcc34a2c1e27f92ef6060566bba
jonybang <jonybang@mail.ru>
2016-01-04 00:17:14 +1000

    ckeditor styles

--- app/assets/javascripts/blogo/admin/ckeditor.js
+++ app/assets/javascripts/blogo/admin/ckeditor.js
@@ -1,4 +1,5 @@
 //= require blogo/ckeditor/ckeditor_basepath
 // = require blogo/ckeditor/ckeditor
+// = require blogo/ckeditor/styles
 //= require blogo/ckeditor/config
 //= require blogo/ckeditor/lang/ru
